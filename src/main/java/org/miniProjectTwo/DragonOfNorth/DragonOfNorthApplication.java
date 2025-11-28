35bdb3c16cb4a7950ddfd005898c48855e5c8fdf
Vinay2080 <shaking.121@gmail.com>
2025-11-28 23:16:41 +0530

    Initial commit Created a project setting up pom.xml

---
+++ src/main/java/org/miniProjectTwo/DragonOfNorth/DragonOfNorthApplication.java
@@ -0,0 +1,13 @@
+package org.miniProjectTwo.DragonOfNorth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonOfNorthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonOfNorthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/miniProjectTwo/DragonOfNorth/DragonOfNorthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miniProjectTwo.DragonOfNorth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonOfNorthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
