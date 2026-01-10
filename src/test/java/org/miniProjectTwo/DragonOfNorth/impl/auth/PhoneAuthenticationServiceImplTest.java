621c1a6c3eb334d3dfdb63e6798819ebd5bbbb48
Vinay2080 <shaking.121@gmail.com>
2026-01-10 11:28:27 +0530

    test: tested get user status for user that does not exist.

--- src/test/java/org/miniProjectTwo/DragonOfNorth/impl/auth/PhoneAuthenticationServiceImplTest.java
+++ src/test/java/org/miniProjectTwo/DragonOfNorth/impl/auth/PhoneAuthenticationServiceImplTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.miniProjectTwo.DragonOfNorth.dto.auth.response.AppUserStatusFinderResponse;
 import org.miniProjectTwo.DragonOfNorth.enums.AppUserStatus;
 import org.miniProjectTwo.DragonOfNorth.enums.IdentifierType;
+import org.miniProjectTwo.DragonOfNorth.model.AppUser;
 import org.miniProjectTwo.DragonOfNorth.repositories.AppUserRepository;
 import org.miniProjectTwo.DragonOfNorth.services.AuthCommonServices;
 import org.mockito.InjectMocks;
@@ -16,6 +17,8 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.miniProjectTwo.DragonOfNorth.enums.AppUserStatus.CREATED;
+import static org.miniProjectTwo.DragonOfNorth.enums.AppUserStatus.NOT_EXIST;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -35,23 +38,30 @@ class PhoneAuthenticationServiceImplTest {
     private PhoneAuthenticationServiceImpl phoneAuthenticationService;
 
 
+    private static final String phoneNumber = "9838291289";
+    private static final String password = "encoded@Password123";
+
     @Test
     void supports_ShouldReturnPHONE_WhenCalled() {
+        // act
         IdentifierType type = phoneAuthenticationService.supports();
 
+        // assert
         assertEquals(IdentifierType.PHONE, type, "PhoneAuthenticationServiceImpl should support PHONE identifier type");
 
     }
 
     @Test
-    void getUserStatus_ShouldReturnStatus_WhenCalled_OtherwiseNOT_EXISTS() {
-        String phoneNumber = "9838291289";
-        AppUserStatus expectedStatus = AppUserStatus.CREATED;
+    void getUserStatus_ShouldReturnStatus_WhenUserExists() {
+        //arrange
+        AppUserStatus expectedStatus = CREATED;
 
         when(appUserRepository.findAppUserStatusByPhone(phoneNumber)).thenReturn(Optional.of(expectedStatus));
 
+        //act
         AppUserStatusFinderResponse response = phoneAuthenticationService.getUserStatus(phoneNumber);
 
+        //assert
         assertNotNull(response, "response should not be null");
         assertEquals(expectedStatus, response.appUserStatus(), "should match the expected value");
         verify(appUserRepository).findAppUserStatusByPhone(phoneNumber);
@@ -59,7 +69,31 @@ class PhoneAuthenticationServiceImplTest {
     }
 
     @Test
-    void signUpUser() {
+    void getUserStatus_ShouldReturnNOT_EXISTS_WhenUserDoesNotExists() {
+        //arrange
+        when(appUserRepository.findAppUserStatusByPhone(phoneNumber)).thenReturn(Optional.empty());
+
+        //act
+        AppUserStatusFinderResponse response = phoneAuthenticationService.getUserStatus(phoneNumber);
+
+        //assert
+        assertNotNull(response, "response should not be null");
+        assertEquals(NOT_EXIST, response.appUserStatus(), "should return NOT_EXISTS for user that does not exists");
+        verify(appUserRepository).findAppUserStatusByPhone(phoneNumber);
+
+    }
+
+    @Test
+    void signUpUser_ShouldReturnStatusCREATED_AndSaveUser_WhenCalled() {
+
+        AppUser appUser = new AppUser();
+        appUser.setPhone(phoneNumber);
+        appUser.setPassword(passwordEncoder.encode(password));
+        appUser.setAppUserStatus(CREATED);
+        appUserRepository.save(appUser);
+
+        assertNotNull(appUser);
+
     }
 
     @Test
