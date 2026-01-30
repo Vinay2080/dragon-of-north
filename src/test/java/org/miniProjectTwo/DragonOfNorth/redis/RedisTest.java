e038b1758f73acccd9f1f510dbcfcc0b85a9aa20
Vinay2080 <shaking.121@gmail.com>
2026-01-31 01:35:07 +0530

    fix(redisTest): trying to use testcontainers to remove need of .env files.
    Signed-off-by: Vinay2080 <shaking.121@gmail.com>

--- src/test/java/org/miniProjectTwo/DragonOfNorth/redis/RedisTest.java
+++ src/test/java/org/miniProjectTwo/DragonOfNorth/redis/RedisTest.java
@@ -6,15 +6,35 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@Testcontainers
 public class RedisTest {
+
+    @Container
+    static GenericContainer<?> redis =
+            new GenericContainer<>("redis:7")
+                    .withExposedPorts(6379);
+
+    @DynamicPropertySource
+    static void redisProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port",
+                () -> redis.getMappedPort(6379));
+    }
 
     private final StringRedisTemplate redisTemplate;
 
