e3af4756fe61d5427b86aaf9c5601f454fda1961
Vinay2080 <shaking.121@gmail.com>
2026-02-20 17:44:24 +0530

    debug(tests): disabled it.
    Signed-off-by: Vinay2080 <shaking.121@gmail.com>

--- src/test/java/org/miniProjectTwo/DragonOfNorth/DragonOfNorthApplicationTests.java
+++ src/test/java/org/miniProjectTwo/DragonOfNorth/DragonOfNorthApplicationTests.java
@@ -1,11 +1,13 @@
 package org.miniProjectTwo.DragonOfNorth;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.miniProjectTwo.DragonOfNorth.integrationTests.BaseIntegrationTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Disabled
 @SpringBootTest
 @ActiveProfiles("test")
 @Testcontainers(disabledWithoutDocker = true)
