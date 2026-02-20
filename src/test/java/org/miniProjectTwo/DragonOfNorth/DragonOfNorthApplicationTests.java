1deb7d550f647876947bfe6ccb631b16d2d8491f
Vinay2080 <shaking.121@gmail.com>
2026-02-20 16:48:51 +0530

    debug(tests): added annotation to skip test when docker is not available.
    Signed-off-by: Vinay2080 <shaking.121@gmail.com>

--- src/test/java/org/miniProjectTwo/DragonOfNorth/DragonOfNorthApplicationTests.java
+++ src/test/java/org/miniProjectTwo/DragonOfNorth/DragonOfNorthApplicationTests.java
@@ -2,6 +2,7 @@ package org.miniProjectTwo.DragonOfNorth;
 
 import org.junit.jupiter.api.Test;
 import org.miniProjectTwo.DragonOfNorth.integrationTests.BaseIntegrationTest;
+import org.miniProjectTwo.DragonOfNorth.integrationTests.EnabledIfDockerAvailable;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -9,6 +10,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @SpringBootTest
 @ActiveProfiles("test")
 @Testcontainers(disabledWithoutDocker = true)
+@EnabledIfDockerAvailable
 class DragonOfNorthApplicationTests extends BaseIntegrationTest {
 
     @Test
