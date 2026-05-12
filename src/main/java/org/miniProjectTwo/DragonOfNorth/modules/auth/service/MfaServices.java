2df3ddf359a689e951d1e1226413298cc67f4cf3
Vinay2080 <shaking.121@gmail.com>
2026-05-12 11:34:12 +0530

    refactor(MfaServices): Updated AuthCommonServiceImpl.java for more readability and modularity separed mfa services and created new class for it.
    Signed-off-by: Vinay2080 <shaking.121@gmail.com>

---
+++ src/main/java/org/miniProjectTwo/DragonOfNorth/modules/auth/service/MfaServices.java
@@ -0,0 +1,14 @@
+package org.miniProjectTwo.DragonOfNorth.modules.auth.service;
+
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+import org.miniProjectTwo.DragonOfNorth.modules.auth.dto.request.AuthRequestContext;
+import org.miniProjectTwo.DragonOfNorth.modules.auth.dto.response.MfaSetupConfirmResponse;
+import org.miniProjectTwo.DragonOfNorth.modules.auth.dto.response.MfaSetupResponse;
+
+public interface MfaServices {
+
+    MfaSetupResponse requestMfaSetup(AuthRequestContext context);
+
+    MfaSetupConfirmResponse confirmMfaSetup(AuthRequestContext context, @NotNull @Length String code);
+}
